6e4df0155161d251f6ef73c271b0fcb4303ae204
kelto <charles.fallourd@gmail.com>
2015-08-17 11:43:32 +0200

    render value of attribute item when the value change

--- docdoku-server/docdoku-server-web/yo/app/js/product-structure/views/configurator/configurator_attribute_item.js
+++ docdoku-server/docdoku-server-web/yo/app/js/product-structure/views/configurator/configurator_attribute_item.js
@@ -17,29 +17,55 @@ define(
                 'click .attribute-remove': 'onRemove'
             },
 
+            initialize: function() {
+                _.bindAll(this);
+                this.item = this.options.item;
+                this.listenTo(this.model,'change', this.renderValues);
+                var name = this.model.get('name');
+                this.listenTo(this.item.model,'change:'+name,this.renderValues);
+            },
+
             render: function () {
-                //TODO kelto: bind values change to updateValue
-                this.$el.html(Mustache.render(template, {i18n: App.config.i18n, model: this.model}));
-                this.bindDOM();
+                this.$el.html(Mustache.render(template, {i18n: App.config.i18n, name: this.model.get('name')}));
+                this.bindDOM().renderValues();
                 return this;
             },
 
             bindDOM: function () {
-                this.attributeValue = this.$('.attribute-value');
+                this.attributeValues = this.$('.attribute-values');
                 this.attributeName = this.$('.attribute-name');
                 return this;
+            },
+
+            renderValues: function() {
+                this.attributeValues.empty();
+                var self = this;
+                var val = this.item.model.get(this.model.get('name'));
+                _.each(this.model.get('operators'), function(operator) {
+                    switch (operator) {
+                        case 'SUM':
+                            self.attributeValues.append('<span> SUM: '+ val);
+                            break;
+                        case 'AVG':
+                            val = val / (self.item.visitedAssemblies + self.item.visitedInstances);
+                            self.attributeValues.append('<span> Average: '+ val);
+                    }
+                    self.attributeValues.append('</span>');
+                });
             },
 
             updateValue: function (value) {
                 // TODO kelto: this method is called on each operation
                 // should only be called at the end
 
-                this.attributeValue.text(value);
+                //this.attributeValue.text(value);
+                this.model.value = value;
+                this.renderValues()
             },
 
 
             onRemove: function() {
-                this.trigger('onRemove',this.model.name);
+                this.item.unset(this.model.get('name'));
             },
 
             remove: function() {
