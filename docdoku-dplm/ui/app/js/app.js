valuekey/docdoku-plm

commit 0bdea3e2737252f3a727e6337f32f3fe7723d7ad

[DPLM] fixed #1000 - added a scope variable called 'downloadFolder' in order to share the download folder in the whole application - added selected in 'Save to' select field of 'part actions' and  'document actions' views in order to print the previous choice of user

docdoku-dplm/ui/app/js/app.js
@@ -84,6 +84,8 @@
                 var currentParts = $location.path().split('/');
                 return currentParts[1] === 'folder'  && folderUuid === currentParts[2];
             };
+
+            $scope.downloadFolder = FolderService.folders.length ? {path: FolderService.folders[0].path } : undefined;
         });
 
 })();

docdoku-dplm/ui/app/js/workspace/document-actions.html
@@ -2,9 +2,9 @@
 <div class="row" ng-if="!document.busy">
     <label>{{'SAVE_TO' | translate}}</label>
     <i class="fa fa-folder-open"></i>
-    <select ng-model="folder.path" class="cursor">
-        <option ng-repeat="folder in folders | orderBy:'-favorite'" value="{{folder.path}}">
-            {{folder.path | fileshortname}}
+    <select ng-model="downloadFolder.path" class="cursor">
+        <option ng-repeat="f in folders | orderBy:'-favorite'" value="{{f.path}}" ng-selected="{{downloadFolder.path == f.path}}">
+            {{f.path | fileshortname}}
         </option>
     </select>
     <label class="hide-input small cursor">

docdoku-dplm/ui/app/js/workspace/part-actions.html
@@ -2,9 +2,9 @@
 <div class="row" ng-if="!part.busy">
     <label>{{'SAVE_TO' | translate}}</label>
     <i class="fa fa-folder-open"></i>
-    <select ng-model="folder.path" class="cursor">
-        <option ng-repeat="folder in folders | orderBy:'-favorite'" value="{{folder.path}}">
-            {{folder.path | fileshortname}}
+    <select ng-model="downloadFolder.path" class="cursor">
+        <option ng-repeat="f in folders | orderBy:'-favorite'" value="{{f.path}}" ng-selected="f.path == downloadFolder.path">
+            {{f.path | fileshortname}}
         </option>
     </select>
     <label class="hide-input small cursor">

commit 918b4a2b384364882b2c69e8cadd1db3c62fe68c

[DPLM] fixed #1000 - fix bug introduced by 0bdea3e2737252f3a727e6337f32f3fe7723d7ad (wrong scope variable name)

docdoku-dplm/ui/app/js/app.js
@@ -85,7 +85,8 @@
                 return currentParts[1] === 'folder'  && folderUuid === currentParts[2];
             };
 
-            $scope.downloadFolder = FolderService.folders.length ? {path: FolderService.folders[0].path } : undefined;
+            $scope.folder = {};
+            $scope.folder = FolderService.folders.length ? {path: FolderService.folders[0].path } : undefined;
         });
 
 })();

docdoku-dplm/ui/app/js/workspace/document-actions.html
@@ -2,8 +2,8 @@
 <div class="row" ng-if="!document.busy">
     <label>{{'SAVE_TO' | translate}}</label>
     <i class="fa fa-folder-open"></i>
-    <select ng-model="downloadFolder.path" class="cursor">
-        <option ng-repeat="f in folders | orderBy:'-favorite'" value="{{f.path}}" ng-selected="{{downloadFolder.path == f.path}}">
+    <select ng-model="folder.path" class="cursor">
+        <option ng-repeat="f in folders | orderBy:'-favorite'" value="{{f.path}}" ng-selected="{{folder.path == f.path}}">
             {{f.path | fileshortname}}
         </option>
     </select>

docdoku-dplm/ui/app/js/workspace/part-actions.html
@@ -2,8 +2,8 @@
 <div class="row" ng-if="!part.busy">
     <label>{{'SAVE_TO' | translate}}</label>
     <i class="fa fa-folder-open"></i>
-    <select ng-model="downloadFolder.path" class="cursor">
-        <option ng-repeat="f in folders | orderBy:'-favorite'" value="{{f.path}}" ng-selected="f.path == downloadFolder.path">
+    <select ng-model="folder.path" class="cursor">
+        <option ng-repeat="f in folders | orderBy:'-favorite'" value="{{f.path}}" ng-selected="f.path == folder.path">
             {{f.path | fileshortname}}
         </option>
     </select>

docdoku-dplm/ui/app/js/workspace/workspace-parts.js
@@ -170,9 +170,6 @@
                         });
                     });
 
-                    $scope.folder = {};
-                    $scope.folder.path = FolderService.folders.length ? FolderService.folders[0].path : '';
-
                     var onError = function(error){
                         $scope.error = error;
                     };